f1b2ae6d933f02d77bba77773b147dabfca2a934
Jake Harris <milieu@javakat.io>
2017-09-16 17:30:23 -0500

    Oldify more stuff

--- static/app.js
+++ static/app.js
@@ -1,29 +1,30 @@
 var colors
-document.addEventListener('DOMContentLoaded', () => {
-    colors = document.querySelectorAll('.color')
+document.addEventListener('DOMContentLoaded', function () {
+    colors = document.getElementsByClassName('color')
 
-    for(let element of colors) {
+    Array.prototype.forEach.call(colors, function (element, i, a) {
         element.addEventListener('touchstart', function(e) {
             e.target.classList.add('touching')
         })
         element.addEventListener('touchend', function(e) {
-            for(let element of colors) {
+            Array.prototype.forEach.call(colors, function (element, i, a) {
                 if(element.classList.contains('touched'))
                     element.classList.remove('touched')
                 if(e.target === element && element.classList.contains('touching'))
                     e.target.classList.add('touched')
                 if(element.classList.contains('touching'))
                     element.classList.remove('touching')
-            }
+            })
         })
         element.addEventListener('click', function(e) {
             console.log(e)
-            var form = new FormData()
+            let form = new FormData()
             form.append('color', e.srcElement.id)
             fetch('/color', {
                 method: 'PUT',
                 body: form
             })
         })
-    }
+        
+    })
 })
